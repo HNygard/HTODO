9d7b68477f83e1362786cc5f2a85055cbf9a78b3
Hallvard Nygard <hallvard.nygard@gmail.com>
2010-10-21 09:49:46 +0200

    Moving functions to separate file

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
-include "configs/mysql.php";
+include 'configs/mysql.php';
+include 'configs/htodo-functions.php';
 
 ?><!doctype html>
 <html>
@@ -37,71 +38,6 @@ while($R = mysql_fetch_assoc($query))
 	$parents[$R['parent']][] = $R['id'];
 	
 	$tasks[$R['id']] = $R;
-}
-
-function print_children($id, $parent_level)
-{
-	global $parents, $tasks;
-	
-	if(isset($parents[$id]))
-	{
-		foreach ($parents[$id] as $children_id)
-		{
-			print_task($tasks[$children_id], $parent_level + 1);
-		}
-	}
-}
-
-function print_task($R, $level)
-{
-	global $tasks;
-	
-	if($R['finished'] == -1)
-		$finisheddisplay = 0;
-	else
-		$finisheddisplay = $R['finished'];
-	
-	if($R['finished'] == 100)
-	{
-		$task_finished_class = 'taskFinished';
-		$finished_class = 'finished';
-	}
-	else
-	{
-		$task_finished_class = 'taskNotfinished';
-		$finished_class = 'notfinished';
-	}
-	
-	if($R['hidden'] == '1')
-	{
-		$hiddenstatus    = 'hidden';
-		$task_css_style  = ' height: 5px; display: list-item; padding: 0px;';
-		$div_css_style   = ' style="display:none;"';
-	}
-	else
-	{
-		$hiddenstatus    = 'nothidden';
-		$task_css_style  = '';
-		$div_css_style   = '';
-	}
-	
-	echo '	<li id="task'.$R['id'].'" style="margin-left: '.(40*$level).'px;'.$task_css_style.'">'.
-		'<div class="sorter"'.$div_css_style.'></div>'.
-		'<div class="finish '.$finished_class.'"'.$div_css_style.'></div>'.
-		'<div class="level"'.$div_css_style.'>'.$level.'</div>'.
-		'<div class="id_display"'.$div_css_style.'>'.$R['id'].'</div>'.
-		'<div class="parent_id"'.$div_css_style.'>'.$R['parent'].'</div>'.
-		'<div class="position"'.$div_css_style.'>'.$R['position'].'</div>'.
-		'<div class="finisheddisplay"'.$div_css_style.'>'.$finisheddisplay.' %</div>'.
-		'<div class="finishedvalue">'.$R['finished'].'</div>'.
-		'<div class="hiddenstatus '.$hiddenstatus.'"></div>'.
-		'<div class="task '.$task_finished_class.'" id="'.$R['id'].'" contenteditable=""'.$div_css_style.'>'.$R['text'].'</div>'.
-	'</li>'.chr(10);
-	
-	// Print the children of this task
-	print_children($R['id'], $level);
-	
-	unset($tasks[$R['id']]);
 }
 
 print_children(0,0); // Start at level 1
